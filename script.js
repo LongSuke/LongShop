ccd26ea914c798d8844a3bfd64af075234e46eef
LongSuke <126839438+LongSuke@users.noreply.github.com>
2023-06-01 07:53:48 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+const bar = document.getElementById('bar');
+const close = document.getElementById('close');
+const nav = document.getElementById('navbar');
+
+if(bar){
+   bar.addEventListener('click',() =>{
+        nav.classList.add('active');
+    })
+}
+
+if(close){
+    close.addEventListener('click',() =>{
+        nav.classList.remove('active');
+    })
+}
+
+const wrapper = document.querySelector('.wrapper');
+const loginLink = document.querySelector('.login-link');
+const registerLink = document.querySelector('.register-link');
+const btnPopup = document.querySelector('.btnLogin-popup');
+
+registerLink.addEventListener('click',()=>{
+    wrapper.classList.add('active');
+});
+
+loginLink.addEventListener('click',()=>{
+    wrapper.classList.remove('active');
+});
+
+btnPopup.addEventListener('click',()=>{
+    wrapper.classList.add('active-popup');
+});
